ad56f356b09c9efae1c9f9092fa06ae8e9a672df
Huang Shewei <huangshewei@gmail.com>
2022-02-16 14:17:06 -0500

    Able to create new restaurants & removed rack mini profile gem

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,7 +9,24 @@ class RestaurantsController < ApplicationController
     # raise
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: "Restaurant created"
+    else
+      render :new
+    end
+  end
+
   private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
