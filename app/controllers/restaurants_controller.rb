33cf55459ea83bb05eb8e4ccbd49a51476e00f4d
Huang Shewei <huangshewei@gmail.com>
2022-02-16 13:52:20 -0500

    Show page is working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,17 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
-    raise
+    # raise
+  end
+
+  def show
+    set_restaurant
+    # raise
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Cleaning the db..."
 Restaurant.destroy_all
 
-puts "Creating 10 restaurants..."
+puts "Creating 5 restaurants..."
 
-10.times do
-  restaurant = Restaurant.create!{
+5.times do
+  restaurant = Restaurant.create!(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
     category: %w[chinese italian japanese french belgian].sample
-  }
+  )
   puts "Restaurant #{restaurant.id} was created"
 end
 
