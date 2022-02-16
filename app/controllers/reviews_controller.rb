033c9f481379be5f0950a4f70bb88186d185bb50
Huang Shewei <huangshewei@gmail.com>
2022-02-16 15:53:04 -0500

    It is possible to add reviews and display it on the show page

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,32 @@
+class ReviewsController < ApplicationController
+  def new
+    # raise
+    set_restaurant
+    @review = Review.new
+  end
+
+  def create
+    set_restaurant
+    @review = Review.new(review_params)
+    # @review.content = "Boring"
+    # raise
+    # How is this line defining th restaurant?
+    @review.restaurant = @restaurant
+    # raise
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  # resources :restaurants
+  resources :restaurants do
+    collection do
+    end
+    resources :reviews, only: [:new, :create]
+  end
 end
